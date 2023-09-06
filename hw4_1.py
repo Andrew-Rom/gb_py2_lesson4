6550bbcf18165b1f009affa179fac19e9ea74f1e
Andrew-Rom <1811mail@gmail.com>
2023-09-06 18:01:37 +0300

    HW 4_1 added additional pre-check for matrix

--- hw4_1.py
+++ hw4_1.py
@@ -4,13 +4,24 @@ HW 4-1
 """
 
 
-def transpose(orig_matrix: list):
-    if all(len(orig_matrix[i]) == len(orig_matrix[i + 1]) for i in range(len(orig_matrix) - 1)):
+def isMatrix(value):
+    if type(value) is list:
+        for i in range(len(value)):
+            if type(value[i]) is not list:
+                return False
+        else:
+            return all(len(value[i]) == len(value[i + 1]) for i in range(len(value) - 1))
+    else:
+        return False
+
+
+def transpose_matrix(orig_matrix):
+    if isMatrix(orig_matrix):
         return [[matrix[row][col] for row in range(len(orig_matrix))] for col in range(len(orig_matrix[0]))]
 
 
-def show_matrix(value: list):
-    if type(value) is list:
+def show_matrix(value):
+    if isMatrix(value):
         print('\n'.join('\t'.join(map(str, row)) for row in value))
     else:
         print('This is not matrix.')
@@ -20,4 +31,4 @@ matrix = [[1, 2, 3], [4, 5, 6]]
 
 show_matrix(matrix)
 print()
-show_matrix(transpose(matrix))
+show_matrix(transpose_matrix(matrix))
